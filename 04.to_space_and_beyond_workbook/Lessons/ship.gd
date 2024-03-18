94d75fa4f9bb3b8f1735224f05a0de47e8a7e030
EvanYee3 <EVYEE@s.sfusd.edu>
2024-03-18 12:40:07 -0700

    M4 L4 Done

--- 04.to_space_and_beyond_workbook/Lessons/ship.gd
+++ 04.to_space_and_beyond_workbook/Lessons/ship.gd
@@ -4,6 +4,10 @@ var max_speed := 600.0
 
 var velocity := Vector2(0, 0)
 
+var boost_speed := 1500.0
+
+var normal_speed := 600.0
+
 func _process(delta:float) -> void:
 	var direction := Vector2(0, 0)
 	direction.x = Input.get_axis("move_left", "move_right")
@@ -12,11 +16,18 @@ func _process(delta:float) -> void:
 	if direction.length() >1.0:
 		direction = direction.normalized()
 	
+	if Input.is_action_just_pressed("boost"):
+		max_speed = boost_speed
+		get_node("Timer").start()
+	
 	velocity = direction * max_speed
 	position += velocity * delta
 	
 	if direction.length() > 0.0:
 		rotation = velocity.angle()
+
+func _on_timer_timeout():
+	max_speed = normal_speed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
