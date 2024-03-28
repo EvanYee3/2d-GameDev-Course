c47a032d878a55c824bb7e72eb21a33144f250f2
EvanYee3 <EVYEE@s.sfusd.edu>
2024-03-28 12:39:07 -0700

    M5 L3 Done

--- 05.loot_it_all_workbook/lessons/health_pack.gd
+++ 05.loot_it_all_workbook/lessons/health_pack.gd
@@ -1,9 +1,7 @@
 extends Area2D
 
+func _ready() -> void:
+ area_entered.connect(_on_area_entered)
+
 func _on_area_entered(area_that_entered: Area2D) -> void:
-	queue_free()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+ queue_free()
