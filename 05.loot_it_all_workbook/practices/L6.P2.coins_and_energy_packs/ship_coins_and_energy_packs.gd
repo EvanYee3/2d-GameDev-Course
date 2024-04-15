2a8787af7b20f98308fc71354d40ecd198cb9ce1
EvanYee3 <EVYEE@s.sfusd.edu>
2024-04-15 11:33:19 -0700

    L6 P1 & P2 Done

--- 05.loot_it_all_workbook/practices/L6.P2.coins_and_energy_packs/ship_coins_and_energy_packs.gd
+++ 05.loot_it_all_workbook/practices/L6.P2.coins_and_energy_packs/ship_coins_and_energy_packs.gd
@@ -10,12 +10,6 @@ var target_position := Vector2(0, 0)
 var coins := 0
 var energy := 20
 
-
-func _ready() -> void:
-	area_entered.connect(_on_area_entered)
-	set_process(false)
-
-
 func set_target_position(new_target_position: Vector2) -> void:
 	target_position = new_target_position
 	set_process(true)
@@ -34,7 +28,21 @@ func _process(delta: float) -> void:
 		set_process(false)
 		target_reached.emit()
 
-
-func _on_area_entered(area_that_entered: Area2D) -> void:
+func set_energy(new_energy: int) -> void:
 	energy += 20.0
 	get_node("UI/EnergyBar").value = energy
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+	set_energy(energy)
+
+func _on_area_entered(area_that_entered: Area2D) -> void:
+	if area_that_entered.is_in_group("energy"):
+		set_energy(energy + 10)
+	elif area_that_entered.is_in_group("coin"):
+		set_coins(coins + 1)
+	#switch something here probably in this function
+
+func set_coins(new_coins: int) -> void:
+	coins = new_coins
+	get_node("UI/CoinsCount").text = "x" + str(coins)
