54d841993d0feff571dc9da97a71a133f43c68f8
EvanYee3 <EVYEE@s.sfusd.edu>
2024-04-19 12:37:05 -0700

    M5 Edits

--- 05.loot_it_all_workbook/practices/L5.P1.code_an_energy_pack/ship_energy_pack.gd
+++ 05.loot_it_all_workbook/practices/L5.P1.code_an_energy_pack/ship_energy_pack.gd
@@ -3,8 +3,13 @@ extends Area2D
 var max_speed := 1200.0
 var velocity := Vector2(0, 0)
 var steering_factor := 10.0
-var health := 10
+
 var energy := 20.0
+
+
+func _ready() -> void:
+	get_node("UI/EnergyBar").value = energy
+
 
 func _process(delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -17,13 +22,6 @@ func _process(delta: float) -> void:
 	if velocity.length() > 0.0:
 		get_node("Sprite2D").rotation = velocity.angle()
 
-func set_energy(new_energy: int) -> void:
-	energy = new_energy
-	get_node("UI/EnergyBar").value = energy
 
-func _ready() -> void:
-	area_entered.connect(_on_area_entered)
-	set_energy(energy)
-
-func _on_area_entered(area_that_entered: Area2D) -> void:
-	set_energy(energy + 20)
+func _on_area_entered(area: Area2D) -> void:
+	pass
--- 05.loot_it_all_workbook/practices/L7.P1.spawn_random_items/random_collectible_placer.gd
+++ 05.loot_it_all_workbook/practices/L7.P1.spawn_random_items/random_collectible_placer.gd
@@ -3,7 +3,7 @@ extends Node2D
 var collectible_scenes := [
 	preload("coin.tscn"),
 	preload("energy_pack.tscn")
-	]
+]
 
 
 func _ready() -> void:
@@ -11,6 +11,4 @@ func _ready() -> void:
 
 
 func _on_timer_timeout() -> void:
-	var random_collectible_scene: PackedScene = collectible_scenes.pick_random()
-	var collectible_instance = random_collectible_scene.instantiate()
-	add_child(collectible_instance)
+	pass
